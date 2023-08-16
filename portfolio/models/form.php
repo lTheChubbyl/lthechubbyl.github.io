bdee4f89bf966975353c12208b309b5a85b85573
TheChubby <miloscubrak@gmail.com>
2023-08-16 22:47:02 +0200

    send mail validation

--- portfolio/models/form.php
+++ portfolio/models/form.php
@@ -1,11 +1,42 @@
 <?php
     header("Content-type: application/json");
+
+    function regExChecker($regEx, $field) {
+        if (!preg_match($regEx, $field)) {
+            return true;
+        }
+    }
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         try {
             foreach ($_POST as $key => $value) {
                 $array[$key] = $value;
             }
             $number = $array["number"] ? $array["number"] : "N/A";
+
+            $errors = array();
+
+            $regExName = "/^[a-zA-Z]+(?:[-' ][a-zA-Z]+)*$/";
+            if (regExChecker($regExName, $array["fname"])) {
+                $errors["fname"] = "Invalid.";
+            }
+            if (regExChecker($regExName, $array["lname"])) {
+                $errors["lname"] = "Invalid.";
+            }
+            $regExEmail = "/^[\w\.-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/";
+            if (regExChecker($regExEmail, $array["email"])) {
+                $errors["email"] = "Invalid.";
+            }
+            if ($number != "N/A") {
+                $regExNumber = "/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{2,6}$/";
+                if (regExChecker($regExNumber, $number)) {
+                    $errors["phone-number"] = "Invalid.";
+                }
+            }
+            $regExMessage = "/^[\s\S]{1,500}$/";
+            if (regExChecker($regExMessage, $array["message"])) {
+                $errors["message"] = "Invalid.";
+            }
 
             $to = "miloscubrak@gmail.com";
             $subject = "Portfolio Contact Form - {$array["fname"]} {$array["lname"][0]}.";
